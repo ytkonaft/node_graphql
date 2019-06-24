3d2c5459333abefab978050933cbc6361be195ea
ytkonaft <boho.ufo@gmail.com>
2019-06-25 00:19:47 +0300

    mutation and temporary events store

--- server.js
+++ server.js
@@ -3,39 +3,63 @@ const bodyParser = require("body-parser");
 const graphQlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+
 const app = express();
+
+const eventsStore = []
 
 app.use(bodyParser.json());
 
 app.get("/", (req, resp, next) => {
   resp.send("Hello");
-});
+}); 
 
 app.use(
   "/graphql",
   graphQlHttp({
     schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String! 
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String! 
+          price: Float!
+          date: String!
+        }
         type RootQuery{
-            events: [String!]!
-            users: [String!]!
+          events: [Event]!
+          users: [String!]!
         }
         type RootMutation {
-            createEvent(name: String!) : String
+          createEvent(eventInput: EventInput): Event
         }
         schema {
-            query: RootQuery
-            mutation:  RootMutation
+          query: RootQuery
+          mutation: RootMutation
         }
     `),
     rootValue: {
       events: () => {
-        return ["test1", "test2", "test3"];
+        return eventsStore;
       },
       users: () => {
         return ["user1", "user2", "user3"];
       },
-      createEvent: ({ name }) => {
-        return name;
+      createEvent: ({eventInput}) => {
+        const event = {
+          _id: Math.random().toString(),
+          title: eventInput.title,
+          description: eventInput.description, 
+          price: +eventInput.price,
+          date: eventInput.date
+        }
+        eventsStore.push(event);
+        return event
       }
     },
     graphiql: true
